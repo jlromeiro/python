62022ef4d3d49437d474478b77f82360674b8deb
eduardo <eduardo.soares@supplylabs.com.br>
2021-10-27 10:32:24 -0300

    exercicio 01

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-msg = "Isto é uma modificação!"
+Tarefa:
+Na raiz do projeto, existe um arquivo chamado emails.txt, faça um programa que:
+- leia este arquivo,
+- tranforme essa lista de emails em um dicionário com chave valor, como exemplo abaixo:
+
+pegar um email, exemplo a_vanessinha_1990@hotmail.com
+
+{
+    nome:"a_vanessinha_1990",
+    email:"a_vanessinha_1990@hotmail.com"
+}
+
+- e por fim, retornar este objeto
